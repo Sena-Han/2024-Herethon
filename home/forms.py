074ad841600774d09ce1a53597e0be8b984a40a3
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-07-04 14:33:26 +0900

    Feat: advice model에 사진 추가

--- home/forms.py
+++ home/forms.py
@@ -5,7 +5,7 @@ from .models import Advice
 class AdviceForm(forms.ModelForm):
     class Meta:
         model = Advice
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'photo']
         widgets = {
             'title': forms.TextInput(attrs={'maxlength': 20}),
             'content': forms.Textarea(attrs={'maxlength': 500}),
--- home/models.py
+++ home/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 
 from accounts.models import CustomUser
@@ -7,8 +9,15 @@ class Advice(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField(max_length=500)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='photos_advice/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(CustomUser, related_name='liked_advices', blank=True)
 
     def like_count(self):
         return self.likes.count()
+
+    def delete(self, *args, **kwargs):
+        if self.photo:
+            if os.path.isfile(self.photo.path):
+                os.remove(self.photo.path)
+        super().delete(*args, **kwargs)
