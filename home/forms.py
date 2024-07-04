bbc6b6c96364183830f4e0a0e93e18718c19a309
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-07-04 12:02:05 +0900

    Feat: advice 모델 추가

---
+++ home/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Advice
+
+
+class AdviceForm(forms.ModelForm):
+    class Meta:
+        model = Advice
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'maxlength': 20}),
+            'content': forms.Textarea(attrs={'maxlength': 500}),
+        }
--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import CustomUser
+
+
+class Advice(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField(max_length=500)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(CustomUser, related_name='liked_advices', blank=True)
+
+    def like_count(self):
+        return self.likes.count()
