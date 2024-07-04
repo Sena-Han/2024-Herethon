104e1e93ec119af1253b7ac984b7918e277ebfcf
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-07-04 12:11:37 +0900

    Feat: 직업 정보 넣었는지 확인하는 함수 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=30, unique=True)
@@ -11,3 +12,7 @@ class CustomUser(AbstractUser):
     job_type = models.CharField(max_length=50, null=True, blank=True)
     start_year = models.IntegerField(null=True, blank=True)
     end_year = models.IntegerField(null=True, blank=True)
+
+    # 임시로 넣어둔 부분입니다. 마이페이지 구현하시면서, 관리자 항목 입력 받는 부분 구현 완료되면 추가할 예정입니다.
+    def is_currently_employed(self):
+        return self.job_type is not None
