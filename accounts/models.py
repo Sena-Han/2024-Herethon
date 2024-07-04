a2d7c2834dd633f1caa757ea0353e64c6283ab88
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-07-04 14:34:54 +0900

    Remove: 임시 함수 is_currently_employed 제거

--- accounts/models.py
+++ accounts/models.py
@@ -12,7 +12,3 @@ class CustomUser(AbstractUser):
     job_type = models.CharField(max_length=50, null=True, blank=True)
     start_year = models.IntegerField(null=True, blank=True)
     end_year = models.IntegerField(null=True, blank=True)
-
-    # 임시로 넣어둔 부분입니다. 마이페이지 구현하시면서, 관리자 항목 입력 받는 부분 구현 완료되면 추가할 예정입니다.
-    def is_currently_employed(self):
-        return self.job_type is not None
